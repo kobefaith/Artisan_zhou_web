f2ae0d85479fa807c7b0cc3e206a2adddd182ede
fei zhou <kobefaith@163.com>
2015-10-20 22:12:14 +0800

    Add the password column to the user model

--- app/models.py
+++ app/models.py
@@ -1,5 +1,5 @@
 from . import db
-
+from werkzeug.security import generate_password_hash,check_password_hash
 
 class Role(db.Model):
     __tablename__ = 'roles'
@@ -7,15 +7,23 @@ class Role(db.Model):
     name = db.Column(db.String(64), unique=True)
     users = db.relationship('User', backref='role', lazy='dynamic')
 
+
     def __repr__(self):
         return '<Role %r>' % self.name
-
 
 class User(db.Model):
     __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), unique=True, index=True)
     role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
-
+    password_hash = db.Column(db.string(128))
     def __repr__(self):
         return '<User %r>' % self.username
+    @property
+    def password(self):
+        raise AttributeError('password is not a readable attribute')
+    @password.setter
+    def password(self, password):
+        self.password_hash = generate_password_hash(password)
+    def verify_password(self,password):
+        return check_password_hash(self.password_hash,password)
