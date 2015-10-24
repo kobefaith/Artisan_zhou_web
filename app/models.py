28f0699db74fbd1c93fdc3173e2f88b933d820af
fei zhou <kobefaith@163.com>
2015-10-24 23:57:43 +0800

    Add the database

--- app/models.py
+++ app/models.py
@@ -19,7 +19,7 @@ class User(UserMixin,db.Model):
     email = db.Column(db.String(64),unique=True,index=True)
     username = db.Column(db.String(64), unique=True, index=True)
     role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
-    password_hash = db.Column(db.string(128))
+    password_hash = db.Column(db.String(128))
     def __repr__(self):
         return '<User %r>' % self.username
     @property
