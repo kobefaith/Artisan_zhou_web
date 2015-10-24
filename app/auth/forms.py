d612383c96eac75b7bafe26c81de228ea046ad8f
fei zhou <kobefaith@163.com>
2015-10-24 23:42:20 +0800

    Add the login view

---
+++ app/auth/forms.py
@@ -0,0 +1,13 @@
+__author__ = 'zhoufei'
+from flask.ext.wtf import Form
+from wtforms import StringField ,PasswordField, BooleanField, SubmitField
+from wtforms.validators import Required, Length, Email
+
+class LoginForm(Form):
+	email = StringField('Email',validators=[Required(),Length(1,64),Email()])
+	password = PasswordField('Password',validators=[Required()])
+	remeber_me = BooleanField('Keep me logged in')
+	submit = SubmitField('Log In')
+
+
+
--- app/models.py
+++ app/models.py
@@ -1,6 +1,7 @@
 from . import db
 from flask.ext.login import UserMixin
 from werkzeug.security import generate_password_hash,check_password_hash
+from . import login_manager
 
 class Role(db.Model):
     __tablename__ = 'roles'
@@ -29,3 +30,9 @@ class User(UserMixin,db.Model):
         self.password_hash = generate_password_hash(password)
     def verify_password(self,password):
         return check_password_hash(self.password_hash,password)
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+
