ae043c88301ce062dccd82816119da948f413ac8
fei zhou <kobefaith@163.com>
2015-10-25 11:28:11 +0800

    Add the test

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -1,13 +1,21 @@
 __author__ = 'zhoufei'
 from flask.ext.wtf import Form
 from wtforms import StringField ,PasswordField, BooleanField, SubmitField
-from wtforms.validators import Required, Length, Email
+from wtforms.validators import Required, Length, Email,Regexp,EqualTo
+from ..models import User
 
 class LoginForm(Form):
 	email = StringField('Email',validators=[Required(),Length(1,64),Email()])
 	password = PasswordField('Password',validators=[Required()])
 	remeber_me = BooleanField('Keep me logged in')
 	submit = SubmitField('Log In')
+class RegistrationForm(Form):
+	email = StringField('Email',validators=[Required(),Length(1,64),Email()])
+	username = StringField('Username',validators=[Required(),Length(1,64),Regexp('^[A-Za-z][A-Za-z0-9_.]*$',0,'Usernames must have only letter,''numbers,dot or underscores')])
+	password = PasswordField('password',validators=[Required(),EqualTo('password2',message='Password must match')])
+	password2 = PasswordField('Confirm password',validators=[Required()])
+	submit = SubmitField('Register')
 
+	def validate_email(self,field):
 
 
