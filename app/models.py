191dd91f01fccdd6399104e95cbf68b72cb27da7
fei zhou <kobefaith@163.com>
2015-10-27 22:47:37 +0800

    Add the token generater

--- app/models.py
+++ app/models.py
@@ -2,6 +2,9 @@ from . import db
 from flask.ext.login import UserMixin
 from werkzeug.security import generate_password_hash,check_password_hash
 from . import login_manager
+from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
+from flask import current_app
+
 
 class Role(db.Model):
     __tablename__ = 'roles'
@@ -20,8 +23,24 @@ class User(UserMixin,db.Model):
     username = db.Column(db.String(64), unique=True, index=True)
     role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
     password_hash = db.Column(db.String(128))
+    confirmed= db.Column(db.Boolean,default=False)
+
     def __repr__(self):
         return '<User %r>' % self.username
+    def generate_confirmation_token(self,expiration=3600):
+        s = Serializer(current_app.config['SECRET_KEY'],expiration)
+        return s.dumps({'confirm':self.id})
+    def confirm(self,token):
+        s = Serializer(current_app.config['SECRET_KEY'])
+        try:
+            data = s.loads(token)
+        except:
+            return False
+        if data.get('confirm') != self.id:
+            return False
+        self.confirmed= True
+        db.session.add(self)
+        return True
     @property
     def password(self):
         raise AttributeError('password is not a readable attribute')
@@ -30,6 +49,7 @@ class User(UserMixin,db.Model):
         self.password_hash = generate_password_hash(password)
     def verify_password(self,password):
         return check_password_hash(self.password_hash,password)
+
 
 @login_manager.user_loader
 def load_user(user_id):
