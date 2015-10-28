1a98aa48fcab41730d34c6deacd1b6201bc92a96
fei zhou <kobefaith@163.com>
2015-10-28 21:26:00 +0800

    Add the reconfirm

--- app/auth/views.py
+++ app/auth/views.py
@@ -49,6 +49,26 @@ def confirm(token):
 	else:
 		flash('The confirmation link is invalid or has expired.')
 	return redirect(url_for('main.index'))
+@auth.before_app_request
+def before_request():
+	if current_user.is_authenticated() \
+		and not current_user.confirmed \
+		and request.endpoint[:5] != 'auth.':
+		and request.endpoint !='static':
+	return redirect(url_for('auth.unconfirmed'))
+@auth.route('/unconfirmed')
+def unconfirmed():
+	if current_user.is_anonymous() or current_user.confirmed:
+		return redirect(url_for('main.index'))
+	return render_template('auth/unconfirmed.html')
+@auth.route('/confirm')
+@login_required
+def resend_confirmation():
+	token = current_user.generate_confirmation_token()
+	send_email(current_user.email,'Confirm Your Account',
+	           'auth/email/confirm',user=current_user,token=token)
+	flash('A new confirmation email has been sent to you by email.')
+	return redirect(url_for('main.index'))
 
 
 
