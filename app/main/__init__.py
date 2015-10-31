da9ff71a8a07abce878a041379aa18fdc7b86f41
fei zhou <kobefaith@163.com>
2015-10-31 17:39:44 +0800

    Add the permission

--- app/main/__init__.py
+++ app/main/__init__.py
@@ -1,5 +1,7 @@
 from flask import Blueprint
 
 main = Blueprint('main', __name__)
-
+@main.app_context_processor
+def inject_permissions():
+	return dict(Permission=Permission)
 from . import views, errors
