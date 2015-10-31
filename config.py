1ab3f3fd06cf15c8970116e22fa73050051776ff
fei zhou <kobefaith@163.com>
2015-10-31 15:10:34 +0800

    change the emial prefix

--- config.py
+++ config.py
@@ -10,8 +10,8 @@ class Config:
     MAIL_USE_TLS = False
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
-    FLASKY_MAIL_SUBJECT_PREFIX = '[Flasky]'
-    FLASKY_MAIL_SENDER = 'Flasky Admin <zhoufei23.good@163.com>'
+    FLASKY_MAIL_SUBJECT_PREFIX = '[Artisan]'
+    FLASKY_MAIL_SENDER = 'Artisan Admin <zhoufei23.good@163.com>'
     FLASKY_ADMIN = os.environ.get('FLASKY_ADMIN')
 
     @staticmethod
