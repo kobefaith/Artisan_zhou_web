106bfe8d593035bb031d1a022d4cab7057a3ce78
fei zhou <kobefaith@163.com>
2015-11-01 10:52:09 +0800

    Add the user infomation

--- app/auth/views.py
+++ app/auth/views.py
@@ -53,11 +53,12 @@ def confirm(token):
 	return redirect(url_for('main.index'))
 @auth.before_app_request
 def before_request():
-	if current_user.is_authenticated \
-			and not current_user.confirmed \
+	if current_user.is_authenticated:
+		current_user.ping()
+		if not current_user.confirmed \
 			and request.endpoint[:5] != 'auth.'\
 			and request.endpoint !='static':
-		return redirect(url_for('auth.unconfirmed'))
+			return redirect(url_for('auth.unconfirmed'))
 @auth.route('/unconfirmed')
 def unconfirmed():
 	if current_user.is_anonymous or current_user.confirmed:
