395feff8a9538e071674d936065c4a291490e757
fei zhou <kobefaith@163.com>
2015-11-03 23:22:50 +0800

    change the main view

--- app/main/views.py
+++ app/main/views.py
@@ -5,8 +5,8 @@ from ..email import send_email
 from . import main
 from .forms import NameForm,EditProfileForm
 from flask.ext.login import current_user,login_user,logout_user,login_required
-
-
+from ..decorators import admin_required,permission_required
+from .forms import EditProfileAdminForm
 @main.route('/', methods=['GET', 'POST'])
 def index():
     form = NameForm()
@@ -47,3 +47,30 @@ def edit_profile():
     form.location.data = current_user.location
     form.about_me.data = current_user.about_me
     return render_template('edit_profile.html',form=form)
+@main.route('/edit-profile/<int:id>',methods=['GET','POST'])
+@login_required
+@admin_required
+def edit_profile_admin(id):
+    user = User.query.get_or_404(id)
+    form = EditProfileAdminForm(user=user)
+    if form.validate_on_submit():
+        user.email = form.email.data
+        user.username = form.username.data
+        user.confirmed = form.confirmed.data
+        user.role = Role.query.get(form.role.data)
+        user.name = form.name.data
+        user.location = form.location.data
+        user.about_me = form.about_me.data
+        db.session.add(user)
+        flash('The Profile has been updated.')
+        return redirect(url_for('.user',username=user.username))
+    form.email.data = user.email
+    form.username.data = user.username
+    form.confirmed.data = user.confirmed
+    form.role.data = user.role_id
+    form.name.data = user.name
+    form.location.data = user.location
+    form.about_me.data = user.about_me
+    return render_template('edit_profile.html',form=form,user=user)
+
+
