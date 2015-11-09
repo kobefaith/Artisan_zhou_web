21f16308a40ad6b6bfdadf4bbe6cc3e2cf761411
fei zhou <kobefaith@163.com>
2015-11-09 22:08:46 +0800

    Add the main view

--- app/main/views.py
+++ app/main/views.py
@@ -1,4 +1,4 @@
-from flask import render_template, session, redirect, url_for, current_app,flash
+from flask import render_template, session, redirect, url_for, current_app,flash,request
 from .. import db
 from ..models import User,Post,Permission
 from ..email import send_email
@@ -19,9 +19,13 @@ def index():
         post = Post(body=form.body.data,author=current_user._get_current_object())
         db.session.add(post)
         return redirect(url_for('.index'))
-    posts = Post.query.order_by(Post.timestamp.desc()).all()
-    return render_template('index.html',form = form,posts = posts)
-
+    page = request.args.get('page', 1, type=int)
+    pagination = Post.query.order_by(Post.timestamp.desc()).paginate(
+        page, per_page=current_app.config['FLASKY_POSTS_PER_PAGE'],
+        error_out=False)
+    posts = pagination.items
+    return render_template('index.html', form=form, posts=posts,
+                           pagination=pagination)
 '''
 def index():
     form = NameForm()
