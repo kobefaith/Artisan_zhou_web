726958b8eac06233e260d4fdb4f574b707f8c895
fei zhou <kobefaith@163.com>
2015-11-11 23:56:59 +0800

    change the post model

--- app/models.py
+++ app/models.py
@@ -5,7 +5,8 @@ from . import login_manager
 from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
 from flask import current_app
 from datetime import datetime
-
+from markdown import markdown
+import bleach
 
 
 class Role(db.Model):
@@ -170,5 +171,15 @@ class Post(db.Model):
     body = db.Column(db.Text)
     timestamp = db.Column(db.DateTime,index=True,default=datetime.utcnow)
     author_id = db.Column(db.Integer ,db.ForeignKey('users.id'))
+    body_html = db.Column(db.Text)
+    @staticmethod
+    def on_changed_body(target,value,oldvalue,initiator):
+        allowed_tags = ['a','abbr','acronym','b','blockquote','code',
+                        'em','i','li','ol','pre','strong','ul',
+                        'h1','h2','h3','p']
+        target.body_html = bleach.linkify(bleach.clean(
+            markdown(value,output_format='html'),
+            tags=allowed_tags,strip=True ))
+    db.event.listen(Post.body,'set',Post.on_changed_body)
 
 
