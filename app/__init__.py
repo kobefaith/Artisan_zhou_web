0b5f0a9c204a74fee9321b86685b2ee879aa9e3b
fei zhou <kobefaith@163.com>
2015-11-11 22:43:01 +0800

    Add the pagedown

--- app/__init__.py
+++ app/__init__.py
@@ -5,6 +5,7 @@ from flask.ext.moment import Moment
 from flask.ext.sqlalchemy import SQLAlchemy
 from config import config
 from flask.ext.login import LoginManager
+from flask.ext.pagedown import PageDown
 
 login_manager = LoginManager()
 login_manager.session_protection = 'strong'
@@ -13,6 +14,7 @@ bootstrap = Bootstrap()
 mail = Mail()
 moment = Moment()
 db = SQLAlchemy()
+pagedown = PageDown()
 
 
 def create_app(config_name):
@@ -24,6 +26,7 @@ def create_app(config_name):
     mail.init_app(app)
     moment.init_app(app)
     db.init_app(app)
+    pagedown.init_app(app)
 
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
