7e623044cfd8459979150813d34b885d1d7badf2
fei zhou <kobefaith@163.com>
2015-11-16 23:34:13 +0800

    add the follow

--- app/models.py
+++ app/models.py
@@ -38,7 +38,13 @@ class Role(db.Model):
         db.session.commit()
     def __repr__(self):
         return '<Role %r>' % self.name
-
+class Follow(db.model):
+    __tablename__='follows'
+    follower_id = db.Column(db.Integer,db.ForeignKey('users.id'),
+                            primary_key=True)
+    followed_id = db.Column(db.Integer,db.ForeignKey('users.id'),
+                            primary_key=True)
+    timestamp = db.Column(db.DateTime,default=datetime.utcnow)
 class User(UserMixin,db.Model):
     __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
@@ -54,6 +60,16 @@ class User(UserMixin,db.Model):
     last_seen = db.Column(db.DateTime(),default=datetime.utcnow)
     image = db.Column(db.String(64))
     posts = db.relationship('Post',backref='author',lazy='dynamic')
+    followed = db.relationship('Follow',
+                               foreign_keys=[Follow.follower_id],
+                               backref=db.backref('follower',lazy='joined')
+                               lazy='dynamic',
+                            cascade='all,delete-orphan')
+    followers = db.relationship('Follow',
+                                foreign_keys=[Follow.followed_id],
+                                backref=db.backref('followed',lazy='joined')
+                                lazy='dynamic',
+                                cascade='all,delete-orphan')
     #image = db.Column(db.LargeBinary())
     def follow(self,user):
         if not self.is_following(user):
