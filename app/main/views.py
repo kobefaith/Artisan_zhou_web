14d7438ab49d4fbba4c741121ff4c4898a0fa69a
fei zhou <kobefaith@163.com>
2015-11-18 22:32:48 +0800

    add the follower view

--- app/main/views.py
+++ app/main/views.py
@@ -155,3 +155,19 @@ def unfollow(username):
     current_user.unfollow(user)
     flash('You are not following %s anymore.' %username)
     return redirect(url_for('.user',username=username))
+@main.route('/followed-by/<username>')
+def followed_by(username):
+    user = User.query.filter_by(username=username).first()
+    if user is None:
+        flash('Invalid user.')
+        return redirect(url_for('.index'))
+    page = request.args.get('page',1,type=int)
+    pagination = user.followed.paginate(
+        page,per_page=current_app.config['ARTISAN_FOLLOWERS_PER_PAGE'],
+        error_out=False  )
+    follows=[{'user':item.followed,'timestamp':item.timestamp}
+             for item in pagination.items]
+    return render_template('followers.html',user=user,title="Followed by",
+                           endpoint='.followed_by',pagination=pagination,
+                           follows=follows)
+
