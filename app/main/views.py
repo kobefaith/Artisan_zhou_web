2cf4f6a8d356c81fbcb4ed0e3d58bf6890b055da
fei zhou <kobefaith@163.com>
2015-11-18 22:09:27 +0800

    add the unfollow view

--- app/main/views.py
+++ app/main/views.py
@@ -141,3 +141,17 @@ def followers(username):
                                          error_out=False)
     follows = [{'user':item.follower,'timestamp':item.timestamp} for item in pagination.items]
     return render_template('followers.html',user=user,title="Followers of",endpoint='.followers',pagination=pagination,follows=follows)
+@main.route('/unfollow/<username>')
+@login_required
+@permission_required(Permission.FOLLOW)
+def unfollow(username):
+    user = User.query.filter_by(username=username).first()
+    if user is None:
+        flash('Invalid user.')
+        return redirect(url_for('.index'))
+    if not current_user.is_following(user):
+        flash('you are not following this user.')
+        return redirect(url_for('.user',username=username))
+    current_user.unfollow(user)
+    flash('You are not following %s anymore.' %username)
+    return redirect(url_for('.user',username=username))
