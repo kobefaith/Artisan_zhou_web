7c2ff1e4cfa7940ee8b6f4ab587aae5493591004
fei zhou <kobefaith@163.com>
2015-11-22 00:15:56 +0800

    add the img view

--- app/main/views.py
+++ app/main/views.py
@@ -1,4 +1,4 @@
-from flask import render_template, session, redirect, url_for, current_app,flash,request
+from flask import render_template, session, redirect, url_for, abort,current_app,flash,request
 from .. import db
 from ..models import User,Post,Permission
 from ..email import send_email
@@ -56,11 +56,14 @@ def index():
                            '''
 @main.route('/user/<username>')
 def user(username):
-    user = User.query.filter_by(username=username).first()
-    if user is None:
-        abort(404)
-    posts = user.posts.order_by(Post.timestamp.desc()).all()
-    return render_template('user.html',user=user,posts=posts)
+    user = User.query.filter_by(username=username).first_or_404()
+    page = request.args.get('page', 1, type=int)
+    pagination = user.posts.order_by(Post.timestamp.desc()).paginate(
+        page, per_page=current_app.config['ARTISAN_POSTS_PER_PAGE'],
+        error_out=False)
+    posts = pagination.items
+    return render_template('user.html', user=user, posts=posts,
+                           pagination=pagination)
 @main.route('/edit-profile',methods=['GET','POST'])
 @login_required
 def edit_profile():
@@ -71,7 +74,9 @@ def edit_profile():
         current_user.about_me = form.about_me.data
         filename = secure_filename(form.image.data.filename)
         form.image.data.save(UPLOAD_FOLDER + filename)
-        current_user.image = APP_ROOT +'/uploads/' + filename
+        #current_user.image = APP_ROOT +'/uploads/' + filename
+        #current_user.image = UPLOAD_FOLDER + filename
+        current_user.image = os.path.join(UPLOAD_FOLDER, filename)
         db.session.add(current_user)
         #db.session.rollback()
         flash('Your Profile has been updated')
