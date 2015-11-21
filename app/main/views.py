cf1ffacac41ac961ef769c49414bf45f830d3183
fei zhou <kobefaith@163.com>
2015-11-21 23:25:36 +0800

    add the set cookie

--- app/main/views.py
+++ app/main/views.py
@@ -20,13 +20,20 @@ def index():
         db.session.add(post)
         return redirect(url_for('.index'))
     page = request.args.get('page', 1, type=int)
-    query = current_user.followed_posts
+    if current_user.is_authenticated:
+        show_followed = bool(request.cookies.get('show_followed', ''))
+    if show_followed:
+        query = current_user.followed_posts
+    else:
+        query = Post.query
     pagination = query.order_by(Post.timestamp.desc()).paginate(
         page, per_page=current_app.config['ARTISAN_POSTS_PER_PAGE'],
         error_out=False)
     posts = pagination.items
     return render_template('index.html', form=form, posts=posts,
                            pagination=pagination)
+
+
 '''
 def index():
     form = NameForm()
@@ -172,3 +179,17 @@ def followed_by(username):
                            endpoint='.followed_by',pagination=pagination,
                            follows=follows)
 
+@main.route('/all')
+@login_required
+def show_all():
+    resp = make_response(redirect(url_for('.index')))
+    resp.set_cookie('show_followed', '', max_age=30*24*60*60)
+    return resp
+
+
+@main.route('/followed')
+@login_required
+def show_followed():
+    resp = make_response(redirect(url_for('.index')))
+    resp.set_cookie('show_followed', '1', max_age=30*24*60*60)
+    return resp
