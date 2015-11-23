5ad7a04b391b05389022ca585b60a0f1fb52ea9b
fei zhou <kobefaith@163.com>
2015-11-23 22:28:58 +0800

    add the comment model

--- app/models.py
+++ app/models.py
@@ -222,4 +222,18 @@ class Post(db.Model):
             tags=allowed_tags,strip=True ))
 db.event.listen(Post.body,'set',Post.on_changed_body)
 
+class Comment(db.Model):
+    __tablename__='comments'
+    id = db.Column(db.Integer,primary_key=True)
+    body = db.Column(db.Text)
+    body_html = db.Column(db.Text)
+    timestamp = db.Column(db.DateTime,index=True,default=datetime.utcnow)
+    disable = db.Column(db.Boolean)
+    author_id = db.Column(db.Integer,db.ForeignKey('users.id'))
+    post_id = db.Column(db.Integer,db.ForeignKey('posts.id'))
+    @staticmethod
+    def on_changed_body(target,value,oldvalue,initiator):
+        allowed_tags = ['a','abbr','acronym','b','code','em','i','strong']
+        target.body_html = bleach.linkify(bleach.clean(markdown(value,output_format='html'),tags=allowed_tags,strip=True))
+db.event.listen(Comment.body,'set',Comment.on_changed_body)
 
