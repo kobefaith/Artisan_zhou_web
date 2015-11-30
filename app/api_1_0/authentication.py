3d01cc08d74f82744953b770ed11a635362f6dfc
fei zhou <kobefaith@163.com>
2015-11-30 22:47:43 +0800

    add the auth error

--- app/api_1_0/authentication.py
+++ app/api_1_0/authentication.py
@@ -24,4 +24,14 @@ def verify_password(email_or_token,password):
 @auth.error_handler
 def auth_error()
 	return unauthorized('Invalid credentials')
-
+@api.before_request
+@auth.login_required
+def before_request()
+	if not g.current_user.is_anonymous and \
+		not g.current_user.confirmed:
+		return forbidden('Unconfirmed account')
+@api.route('/token')
+def get_token():
+	if g.current_user.is_anonymous or g.token_used:
+		return unauthorized('Invalid credentials')
+	return jsonify({'token':g.curreny_user.generate_auth_token(expiration=3600),'expiration': 3600})
