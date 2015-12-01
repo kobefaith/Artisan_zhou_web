408782b5b28324058cf35cad2770250e08b7a892
fei zhou <kobefaith@163.com>
2015-12-01 22:27:13 +0800

    add the post to json

--- app/models.py
+++ app/models.py
@@ -187,6 +187,17 @@ class User(UserMixin,db.Model):
                 user.follow(user)
                 db.session.add(user)
                 db.session.commit()
+    def to_json(self):
+        json_user{
+            'url':url_for('api.get_post',id-self.id,_external=True),
+            'username':self.username,
+            'member_since':self.member_since,
+            'last_seen':self.last_seen,
+            'posts':url_for('aoi.get_user_posts',id=self.id,_external=True),
+            'followed_posts':url_for('api.get_user_followed_posts',id=self.id,_external=True),
+            'post_count':self.posts.count()
+        }
+        return json_user
 
 @login_manager.user_loader
 def load_user(user_id):
