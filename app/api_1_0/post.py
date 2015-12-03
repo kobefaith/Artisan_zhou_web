300b784e8c7a301e01540d2f98a75e49e94938df
fei zhou <kobefaith@163.com>
2015-12-03 22:08:43 +0800

    add the api post

--- app/api_1_0/post.py
+++ app/api_1_0/post.py
@@ -1,1 +1,20 @@
 __author__ = 'zhoufei'
+from flask import jsonify,request,g,abort,url_for,current_app
+from .. import db
+from ..models import Post,Permission
+from . import api
+from .decorators import permission_required
+from .errors import forbidden
+
+@api.route('/posts')
+@auth.login_required
+def get_posts():
+	posts = Post.query.all()
+	return jsonify({'posts':[post.to_json() for post in posts]})
+
+@api.route('/posts/<int:id>')
+@auth.login_required
+def get_post(id):
+	post = Post.query.get_or_404(id)
+	return jsonify(post.to_json())
+
