adbe47d89a2d16e6fbf272e1dee3e60523b9b7a5
fei zhou <kobefaith@163.com>
2015-12-05 16:35:59 +0800

    add the comments

--- app/api_1_0/comments.py
+++ app/api_1_0/comments.py
@@ -1,1 +1,17 @@
 __author__ = 'zhoufei'
+from flask import jsonify,request,g,url_for,current_app
+from .. import db
+from ..models import Post,Permission,Comment
+from . import api
+from .decorators import permisssion_required
+
+@api.route('/comments/')
+def get_comments():
+	page = request.args.get('page',1,type=int)
+	pagination = Comment.query.order_by(Comment.timestamp.desc()).paginate(
+		page,per_page=current_app.config['ARTISAN_COMMENTS_PER_PAGE'],
+		error_out=False	)
+	comments = pagination.items
+	prev = None
+	if pagination.has_prev:
+		prev = url_for('api.get_comments',page=page-1,_external=True)
