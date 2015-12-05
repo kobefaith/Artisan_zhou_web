c4a53f17fa501e503a94de7925a2c54dbc0e029d
fei zhou <kobefaith@163.com>
2015-12-05 17:07:15 +0800

    add the post put method

--- app/api_1_0/post.py
+++ app/api_1_0/post.py
@@ -26,3 +26,13 @@ def new_post():
 	db.session.commit()
 	return jsonify(post.to_json()),201,\
 	       {'location':url_for('api.get_post',id=post.id,_external=True)}
+@api.route('/posts/<int:id>',methods=['PUT'])
+@permission_required(Permission.WRITE_ARTICLES)
+def edit_post(id):
+	post = Post.query.get_or_404(id)
+	if g.current_user != post.author and \
+		not g.current_user.can(Permission.ADMINISTER):
+		return forbidden('Insufficient permissons')
+	post.body = request.json.get('body',post.body)
+	db.session.add(post)
+	return jsonify(post.to_json())
