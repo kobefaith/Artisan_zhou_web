fc0009d580e78a35dd595ff4ee11e0cc217c3011
fei zhou <kobefaith@163.com>
2016-03-01 22:39:48 +0800

    add the change

--- app/api_1_0/authentication.py
+++ app/api_1_0/authentication.py
@@ -22,11 +22,11 @@ def verify_password(email_or_token,password):
 	g.token_used = False
 	return user.verify_password(password)
 @auth.error_handler
-def auth_error()
+def auth_error():
 	return unauthorized('Invalid credentials')
 @api.before_request
 @auth.login_required
-def before_request()
+def before_request():
 	if not g.current_user.is_anonymous and \
 		not g.current_user.confirmed:
 		return forbidden('Unconfirmed account')
