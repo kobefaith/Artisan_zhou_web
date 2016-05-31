c62941b4c09a9877a9ea56cb54c2e46681576af8
fei zhou <kobefaith@163.com>
2016-05-31 23:00:33 +0800

    add

--- app/oauth/test.py
+++ app/oauth/test.py
@@ -16,36 +16,10 @@ renren = oauth.remote_app(
     consumer_key='d6262e9fe5be47adaaeb7fb7a6903e46',
     consumer_secret='143e08f60cb3451bb5e451b068581b66',
     base_url='https://graph.renren.com',
-    request_token_url=None,
-    request_token_params={'scope': 'get_user_info'},
+    request_token_url=None,    
     access_token_url='/oauth/token',
     authorize_url='/oauth/authorize'
 )
-
-
-def json_to_dict(x):
-    '''OAuthResponse class can't not parse the JSON data with content-type
-    text/html, so we need reload the JSON data manually'''
-    if x.find('callback') > -1:
-        pos_lb = x.find('{')
-        pos_rb = x.find('}')
-        x = x[pos_lb:pos_rb + 1]
-    try:
-        return json.loads(x, encoding='utf-8')
-    except:
-        return x
-
-
-def update_renren_api_request_data(data={}):
-    '''Update some required parameters for OAuth2.0 API calls'''
-    defaults = {        
-        'access_token': session.get('renren_token')[0],
-        'oauth_consumer_key': d6262e9fe5be47adaaeb7fb7a6903e46,
-    }
-    defaults.update(data)
-    return defaults
-
-
 @app.route('/')
 def index():
     '''just for verify website owner here.'''
@@ -56,14 +30,11 @@ def index():
 @app.route('/user_info')
 def get_user_info():
     if 'renren_token' in session:
-        return redirect(session['user']['avatar'][0]['url'])
-        #data = update_renren_api_request_data()        
-        #resp = renren.get('/user/get_user_info', data=data)
-        #return jsonify(status=resp.status, data=resp.data)
+        return redirect(session['user']['avatar'][0]['url'])        
     return redirect(url_for('login'))
 
 
-@app.route('/login')
+@app.route('/third_login/renren')
 def login():
     return renren.authorize(callback=url_for('authorized', _external=True))
 
@@ -84,9 +55,7 @@ def authorized():
         )
     session['renren_token'] = (resp['access_token'], '')
 
-    # Get openid via access_token, openid and access_token are needed for API calls
-    #resp = renren.get('/oauth2.0/me', {'access_token': session['renren_token'][0]})
-    resp = json_to_dict(resp.data)
+    # Get openid via access_token, openid and access_token are needed for API calls    
     if isinstance(resp, dict):
         session['user'] = resp.get('user')
     return redirect(url_for('get_user_info'))
